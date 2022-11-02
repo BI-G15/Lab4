c672d88412d2699436d0d5989e2b776452efc5e2
Maria-Gon <69689894+Maria-Gon@users.noreply.github.com>
2022-11-02 18:55:40 -0500

    Correcciones

--- main.py
+++ main.py
@@ -25,18 +25,13 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.post("/predict")
 def make_predictions(dataModel: JsonInput):
-      print("inicio")
       petition_dict = dataModel
-      print("1")
-      print(petition_dict)
       results = proccess(petition_dict)
       return {"result": results}
 
 @app.post("/newData")
 def make_predictions(dataModel: JsonInput2):
-      print("inicio")
       petition_dict = dataModel
-      print(petition_dict)
       dictRest = proccess2(petition_dict)
       return dictRest
 
@@ -44,18 +39,11 @@ def proccess(petition_dict):
    results = []
    for petition in petition_dict:
       for pet in petition[1]:
-         print(pet)
          petition = pet
          df = pd.DataFrame(petition, index=[0])
-         print(df)
-         #df.columns = dataModel.columns()
          model = load("modelo.joblib")
-         print("llego 1")
          result = model.predict(df)
-         print("llego 2")
-         print(result[0])
          results.append(result[0])
-   print(results)
    return results
 
 def proccess2(petition_dict):
@@ -67,7 +55,6 @@ def proccess2(petition_dict):
          df = pd.DataFrame(petition, index=[0])
          frames.append(df.copy())
    df = pd.concat(frames)
-   print(df)
    x = df.drop('Admission Points', axis = 1)
    y = df['Admission Points']
    pipe = model.fit(x,y)
