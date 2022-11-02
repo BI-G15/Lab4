2a7f74eb973965486644fe0199d22025ac1c97c4
Jessica <j.roblesm@uniandes.edu.co>
2022-11-01 20:40:13 -0500

    raro pero medio funciona

--- DataModel.py
+++ DataModel.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
+from typing import List
 
 class DataModel(BaseModel):
 
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
+
     serial_no: float
     gre_score: float
     toefl_score: float
@@ -11,9 +13,11 @@ class DataModel(BaseModel):
     lor: float 
     cgpa: float
     research: float
-    admission_points: float
 
+class JsonInput (BaseModel):
+    print("ok")
+    list_of_inputs: List[dict]
     
 #Esta función retorna los nombres de las columnas correspondientes con el modelo exportado en joblib.
     def columns(self):
-        return ["Serial No.","GRE Score","TOEFL Score","University Rating","SOP","LOR" ,"CGPA","Research","Admission Points"]
+        return ["Serial No.","GRE Score","TOEFL Score","University Rating","SOP","LOR" ,"CGPA","Research"]
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from typing import Optional
 from fastapi import FastAPI
 from joblib import load
 import pandas as pd
+import json
 
 from DataModel import DataModel
+from DataModel import JsonInput
 
 app = FastAPI()
 
@@ -19,9 +21,28 @@ def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
 
 @app.post("/predict")
-def make_predictions(dataModel: DataModel):
-    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
-    df.columns = dataModel.columns()
-    model = load("assets/modelo.joblib")
-    result = model.predict(df)
-    return result
+def make_predictions(dataModel: JsonInput):
+      print("inicio")
+      petition_dict = dataModel
+      print("1")
+      print(petition_dict)
+      results = proccess(petition_dict)
+      return {"result": results}
+
+def proccess(petition_dict):
+   results = []
+   for petition in petition_dict:
+      for pet in petition[1]:
+         print(pet)
+         petition = pet
+         df = pd.DataFrame(petition, index=[0])
+         print(df)
+         #df.columns = dataModel.columns()
+         model = load("assets/modelo.joblib")
+         print("llego 1")
+         result = model.predict(df)
+         print("llego 2")
+         print(result[0])
+         results.append(result[0])
+   print(results)
+   return results
